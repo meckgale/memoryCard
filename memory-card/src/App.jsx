97c0ea0109d889370a664e60266cb72ced83e23c
meck <meckgalen@gmail.com>
2024-02-01 16:10:11 +0300

    shuffle and highscore features implemented

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -8,21 +8,50 @@ const objList = await pokemonList();
 export default function App() {
   const [count, setCount] = useState(0);
   const [clickedCards, setClickedCards] = useState([]);
+  const [highscore, setHighscore] = useState(null);
+
+  function handleHighscore() {
+    count >= highscore ? setHighscore(count) : highscore;
+  }
+
+  function shuffle() {
+    let currentIndex = objList.length,
+      randomIndex;
+
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [objList[currentIndex], objList[randomIndex]] = [
+        objList[randomIndex],
+        objList[currentIndex],
+      ];
+    }
+    return objList;
+  }
+
+  function newTurn() {
+    setCount(0);
+    setClickedCards([]);
+    shuffle();
+    handleHighscore();
+  }
 
   function handleCard(obj) {
-    if (clickedCards.includes(obj.id)) return;
+    if (clickedCards.includes(obj.id)) return newTurn();
     setCount((c) => c + 1);
     setClickedCards(() => [...clickedCards, obj.id]);
-    console.log(clickedCards);
+    shuffle();
   }
 
   return (
     <div className="main">
       <header className="header-container">
-        <h1>Title of the App</h1>
+        <h1>Remember Your Pokecards</h1>
         <div>
           <h1>Scoreboard</h1>
-          <h3>{count}</h3>
+          <h4>{highscore && `Highscore: ${highscore}`}</h4>
+          <h3>Score: {count}</h3>
         </div>
       </header>
       <CardList onHandle={handleCard} objList={objList} />
